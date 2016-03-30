1a76d932e61721dd3b1581f083f99a68fff0f1cb
Sam Wilson <sam@samwilson.id.au>
2016-03-30 20:52:04 +0800

    Line endings?

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,7 @@
 // Make sure Composer has been set up (for installation from Git, mostly).
 if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
     echo '<p>Please run <tt>composer install</tt> prior to using Tabulate.</p>';
-    return;
+    exit(1);
 }
 require __DIR__ . '/vendor/autoload.php';
 
